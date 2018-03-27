7e242ddfbab9a337fcfd454e6789d4228ed53166
abdulharbi <37831037+abdulharbi@users.noreply.github.com>
2018-03-27 12:29:34 +0300

    MyVaribles
    this is second Program in C#

---
+++ MyVaribles/MyVaribles/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyVaribles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myValue =7;
+            Console.WriteLine("{0}", myValue);
+            Console.ReadKey();
+        }
+    }
+}
